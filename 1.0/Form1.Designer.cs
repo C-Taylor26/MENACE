f57597e052e132ee4dfcd57451bcc4ed1aa0ec66
Callumt683 <44776928+Callumt683@users.noreply.github.com>
2020-01-20 17:41:02 +0000

    Completed Project
    Completed before using Github

---
+++ 1.0/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace _1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pos1 = new System.Windows.Forms.TextBox();
+            this.pos2 = new System.Windows.Forms.TextBox();
+            this.pos3 = new System.Windows.Forms.TextBox();
+            this.pos4 = new System.Windows.Forms.TextBox();
+            this.pos5 = new System.Windows.Forms.TextBox();
+            this.pos6 = new System.Windows.Forms.TextBox();
+            this.pos7 = new System.Windows.Forms.TextBox();
+            this.pos8 = new System.Windows.Forms.TextBox();
+            this.pos9 = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.cbLearning = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumGames = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbShowUpdates = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // pos1
+            // 
+            this.pos1.Location = new System.Drawing.Point(12, 63);
+            this.pos1.Name = "pos1";
+            this.pos1.Size = new System.Drawing.Size(32, 20);
+            this.pos1.TabIndex = 0;
+            // 
+            // pos2
+            // 
+            this.pos2.Location = new System.Drawing.Point(50, 63);
+            this.pos2.Name = "pos2";
+            this.pos2.Size = new System.Drawing.Size(32, 20);
+            this.pos2.TabIndex = 1;
+            // 
+            // pos3
+            // 
+            this.pos3.Location = new System.Drawing.Point(88, 63);
+            this.pos3.Name = "pos3";
+            this.pos3.Size = new System.Drawing.Size(32, 20);
+            this.pos3.TabIndex = 2;
+            // 
+            // pos4
+            // 
+            this.pos4.Location = new System.Drawing.Point(12, 89);
+            this.pos4.Name = "pos4";
+            this.pos4.Size = new System.Drawing.Size(32, 20);
+            this.pos4.TabIndex = 3;
+            // 
+            // pos5
+            // 
+            this.pos5.Location = new System.Drawing.Point(50, 89);
+            this.pos5.Name = "pos5";
+            this.pos5.Size = new System.Drawing.Size(32, 20);
+            this.pos5.TabIndex = 4;
+            // 
+            // pos6
+            // 
+            this.pos6.Location = new System.Drawing.Point(88, 89);
+            this.pos6.Name = "pos6";
+            this.pos6.Size = new System.Drawing.Size(32, 20);
+            this.pos6.TabIndex = 5;
+            // 
+            // pos7
+            // 
+            this.pos7.Location = new System.Drawing.Point(12, 115);
+            this.pos7.Name = "pos7";
+            this.pos7.Size = new System.Drawing.Size(32, 20);
+            this.pos7.TabIndex = 6;
+            // 
+            // pos8
+            // 
+            this.pos8.Location = new System.Drawing.Point(50, 115);
+            this.pos8.Name = "pos8";
+            this.pos8.Size = new System.Drawing.Size(32, 20);
+            this.pos8.TabIndex = 7;
+            // 
+            // pos9
+            // 
+            this.pos9.Location = new System.Drawing.Point(88, 115);
+            this.pos9.Name = "pos9";
+            this.pos9.Size = new System.Drawing.Size(32, 20);
+            this.pos9.TabIndex = 8;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(12, 152);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(108, 23);
+            this.btnPlay.TabIndex = 9;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 199);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(108, 20);
+            this.txtResult.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Games Played:\r\n";
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.Location = new System.Drawing.Point(140, 63);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(122, 173);
+            this.lstResults.TabIndex = 13;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(121, 243);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(141, 20);
+            this.txtTotal.TabIndex = 14;
+            // 
+            // cbLearning
+            // 
+            this.cbLearning.AutoSize = true;
+            this.cbLearning.Location = new System.Drawing.Point(12, 246);
+            this.cbLearning.Name = "cbLearning";
+            this.cbLearning.Size = new System.Drawing.Size(103, 17);
+            this.cbLearning.TabIndex = 15;
+            this.cbLearning.Text = "Enable Learning";
+            this.cbLearning.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtNumGames
+            // 
+            this.txtNumGames.Location = new System.Drawing.Point(12, 29);
+            this.txtNumGames.Name = "txtNumGames";
+            this.txtNumGames.Size = new System.Drawing.Size(178, 20);
+            this.txtNumGames.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Enter how many games to be played\r\n";
+            // 
+            // cbShowUpdates
+            // 
+            this.cbShowUpdates.AutoSize = true;
+            this.cbShowUpdates.Location = new System.Drawing.Point(12, 265);
+            this.cbShowUpdates.Name = "cbShowUpdates";
+            this.cbShowUpdates.Size = new System.Drawing.Size(170, 17);
+            this.cbShowUpdates.TabIndex = 19;
+            this.cbShowUpdates.Text = "Show Updates (Slows runtime)";
+            this.cbShowUpdates.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 288);
+            this.Controls.Add(this.cbShowUpdates);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNumGames);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbLearning);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.pos9);
+            this.Controls.Add(this.pos8);
+            this.Controls.Add(this.pos7);
+            this.Controls.Add(this.pos6);
+            this.Controls.Add(this.pos5);
+            this.Controls.Add(this.pos4);
+            this.Controls.Add(this.pos3);
+            this.Controls.Add(this.pos2);
+            this.Controls.Add(this.pos1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pos1;
+        private System.Windows.Forms.TextBox pos2;
+        private System.Windows.Forms.TextBox pos3;
+        private System.Windows.Forms.TextBox pos4;
+        private System.Windows.Forms.TextBox pos5;
+        private System.Windows.Forms.TextBox pos6;
+        private System.Windows.Forms.TextBox pos7;
+        private System.Windows.Forms.TextBox pos8;
+        private System.Windows.Forms.TextBox pos9;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.CheckBox cbLearning;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumGames;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox cbShowUpdates;
+    }
+}
+
---
+++ 1.0/Form1.cs
@@ -0,0 +1,528 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1._0
+{
+    public partial class Form1 : Form   
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void btnPlay_Click(object sender, EventArgs e)
+        {
+            cbLearning.Hide();
+            btnPlay.Hide();
+            List list = new List();
+            int x = 0;
+            int o = 0;
+            int d = 0;
+            lstResults.Items.Clear();
+            //check number of games to be played
+            string strNumGames = txtNumGames.Text;
+            int numGames;
+            //check not null and value
+            bool isInt = int.TryParse(strNumGames, out numGames);
+            if (numGames > 0)
+            {
+                for (int j = 0; j < numGames; j++) //Each loop is one comepleted game
+                {
+                    list = reset(list);
+
+                    int choice;
+                    {
+                        //txtTotal.Clear();
+                        txtResult.Clear();
+                        pos1.Clear();
+                        pos2.Clear();
+                        pos3.Clear();
+                        pos4.Clear();
+                        pos5.Clear();
+                        pos6.Clear();
+                        pos7.Clear();
+                        pos8.Clear();
+                        pos9.Clear();
+                    }//Clears form
+                    bool nFirst = false;
+                    //decide who should go first based of numGames
+                    if (j % 2== 0)
+                    {
+                        nFirst = true;
+                    }
+                    do //echo loop is a turn for both O's and X's
+                    {
+                        //gameEnd() checked each turn 
+                        if (nFirst == true)
+                        {
+                            choice = turnN(); //Gets choice
+                            takeTurn('O', choice); //Adds choice to the game board
+                            if (cbShowUpdates.Checked == true)
+                            {
+                                Application.DoEvents(); //used to show the updates as each move is played
+                            }
+                            if (gameEnd() == true)
+                                break;
+
+                            choice = turnC(list);
+                            list = saveState(list, choice);
+                            takeTurn('X', choice);
+
+                            if (cbShowUpdates.Checked == true)
+                            {
+                                Application.DoEvents();
+                            }
+                        }
+                        else
+                        {
+                            choice = turnC(list);
+                            list = saveState(list, choice);
+                            takeTurn('X', choice);
+
+                            if (cbShowUpdates.Checked == true)
+                            {
+                                Application.DoEvents();
+                            }
+                            if (gameEnd() == true)
+                                break;
+                            choice = turnN();
+                            takeTurn('O', choice);
+                            if (cbShowUpdates.Checked == true)
+                            {
+                                Application.DoEvents();
+                            }
+                        }
+                        
+                    }
+                    while (gameEnd() == false);
+
+                    txtResult.Text = checkWin(); //lets player know who won the round
+                    lstResults.Items.Add(txtResult.Text); //Displays result
+                    label2.Text = "Games Played: " + (j + 1); 
+                    txtTotal.Text = "X: " + x + ", O: " + o + ", D: " + d;
+
+                    if (txtResult.Text == "X's Win!")
+                    {
+                        x++; 
+                        list = learning(list, 2);
+                    }
+                    else if (txtResult.Text == "O's Win!")
+                    {
+                        o++;
+                        list = learning(list, -1);
+                    }
+                    else
+                    {
+                        d++;
+                        list = learning(list, 1);
+                    }
+                }
+                txtTotal.Text = "X: " + x + ", O: " + o + ", D: " + d;
+                cbLearning.Show();
+                btnPlay.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please enter an int > 0");
+            }
+            
+        }
+
+        public int turnN()
+        {
+            //Naughts are a random player
+            int choice;
+
+            Random rand = new Random();
+            do //gets random turn
+            {
+                choice = rand.Next(1, 10);
+            }
+            while (getChar(choice) != "");//checks that the turn is valid and not already taken
+
+            return choice;
+        }
+
+        public int turnC(List list)
+        {
+            int count = 0;
+            int choice =0;
+            Random rand = new Random();
+            GameState currentState = getState(list);
+            int[] choices = currentState.moves;
+            int num = choices[0];
+            int[] moves = new int[9];
+
+            //finds best move from moves that have worked previously
+            for (int i = 0; i < choices.Length; i++)
+            {
+                if (choices[i] > num)
+                {
+                    num = choices[i];
+                }
+            }
+            //adds all the best to moves[]
+            for (int i = 0; i < choices.Length; i++)
+            {
+                if (choices[i] == num )
+                {
+                    moves[i] = 1;
+                }
+            }
+            //selects a space that is in moves[] and is not taken
+            do
+            {
+                do
+                {
+                    choice = rand.Next(1, 10);
+                    count++;
+                } while (getChar(choice) != "");
+                if (count > 500)
+                {
+                    break;
+                }
+            } while (moves[choice - 1] != 1);
+
+            return choice;
+        }
+
+        public bool gameEnd()
+        {
+            int count = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                if (getChar(i) != "")
+                {
+                    count++;
+                }
+            }
+            if (count == 9)//ends game if all spaces are full
+            {
+                return true;
+            }
+            else if (checkWin() != "Its a draw")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public string checkWin()
+        {
+            //checks the various ways in which a player could win
+            string result = "";
+            if (pos1.Text == pos2.Text && pos2.Text == pos3.Text
+                || pos1.Text == pos4.Text && pos4.Text == pos7.Text
+                || pos1.Text == pos5.Text && pos5.Text == pos9.Text)
+            {
+                if (pos1.Text != "")
+                {
+                    result = pos1.Text + "'s Win!"; //return the winning character (X/O)
+                    return result;
+                }
+
+            }
+            else if (pos4.Text == pos5.Text && pos5.Text == pos6.Text
+                    || pos2.Text == pos5.Text && pos5.Text == pos8.Text
+                    || pos3.Text == pos5.Text && pos5.Text == pos7.Text)
+            {
+                if (pos5.Text != "")
+                {
+                    result = pos5.Text + "'s Win!";
+                    return result;
+                }
+            }
+            else if (pos7.Text == pos8.Text && pos8.Text == pos9.Text
+                    || pos3.Text == pos6.Text && pos6.Text == pos9.Text)
+            {
+                if (pos9.Text != "")
+                {
+                    result = pos9.Text + "'s Win!";
+                    return result;
+                }
+
+            }
+
+            return "Its a draw"; //no winning outcome found
+        }
+
+        public void takeTurn(char a, int choice)
+        {
+            switch (choice) //places charachter in chosen posistion
+            {
+                case 1:
+                    pos1.Text = Convert.ToString(a);
+                    break;
+                case 2:
+                    pos2.Text = Convert.ToString(a);
+                    break;
+                case 3:
+                    pos3.Text = Convert.ToString(a);
+                    break;
+                case 4:
+                    pos4.Text = Convert.ToString(a);
+                    break;
+                case 5:
+                    pos5.Text = Convert.ToString(a);
+                    break;
+                case 6:
+                    pos6.Text = Convert.ToString(a);
+                    break;
+                case 7:
+                    pos7.Text = Convert.ToString(a);
+                    break;
+                case 8:
+                    pos8.Text = Convert.ToString(a);
+                    break;
+                case 9:
+                    pos9.Text = Convert.ToString(a);
+                    break;
+            }
+        }
+
+        public List saveState(List list, int choice)//Finds excisting game state or saves it and returns the state object
+        {
+            GameState currentState = getState(list);
+            currentState.used = true;
+            currentState.posPlayed = choice;
+            return list;
+        }
+
+        public GameState getState(List list)//returns the possible moves that can be made in current state
+        {
+
+            GameState currentState = new GameState();
+            GameState checkState = list.firstNode;
+            int[] state = new int[9];
+
+            for (int i = 0; i < state.Length; i++) //finds the state of the board
+            {
+                if (getChar(i + 1) == "")
+                {
+                    state[i] = 0; //No entry in position 
+                }
+                else
+                {
+                    if (getChar(i + 1) == "X")
+                    {
+                        state[i] = 1; //X in posistion
+                    }
+                    else
+                    {
+                        state[i] = 2; //O in position
+                    }
+                }
+            }
+            currentState.state = state;
+
+            currentState.moves = getMoves();
+
+            //find if state[] already exists within list
+            if (list.firstNode == null)//nothing in list
+            {
+                list.firstNode = currentState;
+            }
+            else
+            {
+                //search list for state
+                while (checkState != null)
+                {
+                    if (compare(checkState, state) == true)//when found CurrentState becomes the matching game state
+                    {
+                        currentState = checkState;
+                        return currentState;
+                    }
+
+                    checkState = checkState.next;
+                }
+                //no state found so currentState is added to the list
+                insertBeginning(list, currentState);
+                return currentState;
+            }
+            return currentState;
+        }
+
+        public string getChar(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    return pos1.Text;
+
+                case 2:
+                    return pos2.Text;
+
+                case 3:
+                    return pos3.Text;
+
+                case 4:
+                    return pos4.Text;
+
+                case 5:
+                    return pos5.Text;
+
+                case 6:
+                    return pos6.Text;
+
+                case 7:
+                    return pos7.Text;
+
+                case 8:
+                    return pos8.Text;
+
+                case 9:
+                    return pos9.Text;
+            }
+            return null;
+        }
+
+        public int[] getMoves()//function makes new array of possible moves
+        {
+            int[] moves = new int[9];
+            string text = "";
+
+            for (int i = 0; i < 9; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        text = pos1.Text;
+                        break;
+                    case 1:
+                        text = pos2.Text;
+                        break;
+                    case 2:
+                        text = pos3.Text;
+                        break;
+                    case 3:
+                        text = pos4.Text;
+                        break;
+                    case 4:
+                        text = pos5.Text;
+                        break;
+                    case 5:
+                        text = pos6.Text;
+                        break;
+                    case 6:
+                        text = pos7.Text;
+                        break;
+                    case 7:
+                        text = pos8.Text;
+                        break;
+                    case 8:
+                        text = pos9.Text;
+                        break;
+                }
+
+                if (text == "")
+                {
+                    moves[i] = 1;//position is avalible
+                }
+                else
+                {
+                    moves[i] = 0;//posistion is taken
+                }
+            }
+
+
+            return moves;
+        }
+
+        public void insertBeginning(List list, GameState newState)
+        {
+            if (list.firstNode == null)
+            {
+                list.firstNode = newState;
+            }
+            else
+            {
+                newState.next = list.firstNode;
+                list.firstNode = newState;
+            }
+        }
+
+        public List learning(List list, int outcome)
+        {
+            if (cbLearning.Checked == true)
+            {
+                //search for "used == y"
+                GameState currentState = list.firstNode;
+
+                while (currentState != null)
+                {
+                    if (currentState.used == true)
+                    {
+                        currentState.moves[currentState.posPlayed - 1] = currentState.moves[currentState.posPlayed - 1] + outcome;
+                    }
+                    currentState = currentState.next;
+                }
+            }
+            return list;
+        }
+
+        public List reset(List list)
+        {
+            GameState state = list.firstNode;
+
+            while (state != null)
+            {
+                state.used = false;
+                state.posPlayed = 0;
+                state = state.next;
+            }
+
+
+            return list;
+        }
+
+        public bool compare(GameState checkState, int[] state)
+        {
+            int matches = 0;
+            for (int s = 0; s < state.Length; s++)
+            {
+                if(checkState.state[s] == state[s])
+                {
+                    matches++;
+                }
+            }
+            
+            if (matches == 9)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+    public class List
+    {
+        public GameState firstNode;
+    }
+
+    public class GameState
+    {
+        public bool used;
+        public int posPlayed;
+        public int[] state;
+        public int[] moves;
+        public GameState next;
+    }
+}
